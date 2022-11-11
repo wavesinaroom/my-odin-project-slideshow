8ed5d6b0d6f902e6fd31ca181213677de5ffba8e
wavesinaroom <juan@wavesinaroom.com>
2022-11-11 11:28:12 -0500

    [Feature] Cycle through frame divs with ui buttons

--- slide-show.js
+++ slide-show.js
@@ -1,8 +1,10 @@
-slides = [...document.querySelectorAll(".frame")];
+const slides = [...document.querySelectorAll(".frame")];
+let pointer = 0;
 
 function changeSlide(pointer) {
   slides.forEach((slide) => {
-    if (slide.id === pointer) {
+    console.log(pointer);
+    if (slide.id === pointer.toString()) {
       slide.style.color ='red';
     } else {
       slide.style.color = 'blue';
@@ -10,4 +12,30 @@ function changeSlide(pointer) {
   });
 }
 
-changeSlide("three");
+function goToNextSlide(){
+  if(pointer===slides.length-1){
+    pointer = 0;
+    return pointer;
+  }else{
+    return pointer+=1;
+  }
+}
+
+function goToPreviousSlide(){
+  if(pointer===0){
+    pointer = slides.length-1;
+    return pointer; 
+  }else{
+    return pointer-=1;
+  }
+}
+
+document.getElementById('next').addEventListener('click', ()=>{
+  changeSlide(goToNextSlide());
+});
+
+document.getElementById('previous').addEventListener('click', ()=>{
+  changeSlide(goToPreviousSlide());
+});
+
+changeSlide('2');
